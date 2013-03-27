dac7a5f9f0d2173a3b7053df6ac069aff141e1d8
Philipp <fsfeel@gmail.com>
2013-03-27 22:08:13 +0400

    improve build

--- build.py
+++ build.py
@@ -1,53 +1,62 @@
 import bold
-from bold import build_path
-import os
-import sys
+from bold import build_path, util
 import multiprocessing
+import shutil
 
 
 class Libav (bold.builders.Builder):
-	src_path = 'src/libav/'
-	target = src_path + 'libavformat/libavformat.a'
 	required_by = lambda: Rtmp_load.target
-	sources = src_path + 'VERSION'
+
+	src_path = 'src/libav'
+	sources = lambda self: list(util.get_file_paths_recursive(self.src_path))
+	src_copy_path = build_path + 'libav_src'
+	target = src_copy_path + '/libavformat/libavformat.a'
 
 	def build (self, _changed_targets, _src_paths):
-		with bold.util.change_cwd(self.src_path):
+		shutil.rmtree(self.resolve(self.src_copy_path), ignore_errors = True)
+		shutil.copytree(self.src_path, self.resolve(self.src_copy_path))
+
+		with util.change_cwd(self.resolve(self.src_copy_path)):
 			#--disable-debug --enable-gpl --enable-nonfree
 			self.shell_run("./configure --enable-librtmp --disable-doc --disable-ffmpeg --disable-avconv"
 				" --disable-avplay --disable-avprobe --disable-avserver --disable-swscale --disable-avdevice"
 				" --disable-avfilter --enable-runtime-cpudetect --disable-encoders"
-				" && make --jobs %i" % multiprocessing.cpu_count())
+				" && make --jobs %i" % (multiprocessing.cpu_count() * 2))
 		self._update_target(self.target)
 
 class Luajit (bold.builders.Builder):
-	src_path = 'src/luajit'
-	target = src_path + '/src/libluajit.a'
 	required_by = lambda: Rtmp_load.target
-	sources = lambda self: list(bold.util.get_file_paths_recursive(self.src_path, [self.src_path + '/doc/*']))
+
+	src_path = 'src/luajit'
+	src_copy_path = build_path + 'luajit_src'
+	target = src_copy_path + '/src/libluajit.a'
+	sources = lambda self: list(util.get_file_paths_recursive(self.src_path, [self.src_path + '/doc/*']))
 
 	def build (self, _changed_targets, _src_paths):
-		with bold.util.change_cwd(self.src_path):
+		shutil.rmtree(self.resolve(self.src_copy_path), ignore_errors = True)
+		shutil.copytree(self.src_path, self.resolve(self.src_copy_path))
+
+		with util.change_cwd(self.resolve(self.src_copy_path)):
 			self.shell_run('''make amalg CCDEBUG=" -g" BUILDMODE=" static"''')
 		self._update_target(self.target)
 
 class Rtmp_load (bold.builders.CProgram):
 	target = build_path + 'rtmp_load'
 	sources = 'src/*.c'
-	compile_flags = '-O3 -g -std=gnu99 -Wall -pthread -DUSE_SYSEXITS'  # USE_SYSEXITS is for gopt
+	deps_include_missing_header = True
+	gopt_flags = '-DUSE_SYSEXITS'
+	compile_flags = '-O3 -g -std=gnu99 -Wall -pthread ' + gopt_flags
 	includes = [
-		# '/home/f/proj/libav-0.8.3/i/include/',
-		Libav.src_path,
+		Libav.src_copy_path,
 		# '/usr/include/lua5.1/',
 		Luajit.src_path + '/src',
 	]
 	link_flags = '-Wl,--export-dynamic' #-Wl,-rpath=/home/f/proj/libav-0.8.3/i/lib/
 	lib_paths = [
-		# '/home/f/proj/libav-0.8.3/i/lib/',
-		Libav.src_path + 'libavformat',
-		Libav.src_path + 'avcodec',
-		Libav.src_path + 'avutil',
-		Luajit.src_path + '/src',
+		Libav.src_copy_path + '/libavformat',
+		Libav.src_copy_path + '/libavcodec',
+		Libav.src_copy_path + '/libavutil',
+		Luajit.src_copy_path + '/src',
 	]
 	libs = [
 		'rt',
@@ -56,12 +65,14 @@ class Rtmp_load (bold.builders.CProgram):
 		'avutil',
 		'rtmp',
 		'luajit',
+		'm',
 	]
 
 class FFIHeader (bold.builders.Builder):
+	required_by = Rtmp_load.target
+
 	sources = 'ffi.py', 'src/run.h'
 	target = build_path + 'luajit_ffi.h'
-	required_by = Rtmp_load.target
 
 	def build (self, changed_targets, src_paths):
 		headers = "\n".join([
